cad7545acf606b621872907ba7f9056196d470c6
JohnMarston <josalbarr@alu.edu.gva.es>
2024-02-23 19:47:11 +0100

    asd

--- final/numbersTest.java
+++ final/numbersTest.java
@@ -1,13 +1,10 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-import JUnit_Proyect.numbers;
+import JUnit_Proyect.booleans;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 class numbersTest {
-
     @Test
     public void mamada(){
 
-
     }
-
 }
